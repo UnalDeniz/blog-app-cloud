944d4fdb56e6c2d35105b298c5ea8d215535c8ee
Deniz Ünal <den1z.unal@protonmail.com>
2024-11-23 21:16:31 +0300

    changed the database to postgresql

--- blog_backend/blog_backend/settings.py
+++ blog_backend/blog_backend/settings.py
@@ -14,6 +14,11 @@ import os
 from pathlib import Path
 from datetime import timedelta
 
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -149,8 +154,12 @@ WSGI_APPLICATION = 'blog_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST', default='localhost'),
+        'PORT': env('DB_PORT', default='5432'),
     }
 }
 
